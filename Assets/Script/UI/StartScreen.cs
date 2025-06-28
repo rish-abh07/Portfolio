f0181e186cbf23571f2aac06ee27bd61330a3ee8
Rishabh Pal <rish.09.rp@gmail.com>
2025-06-28 12:07:51 +0530

    initaition

---
+++ Assets/Script/UI/StartScreen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UIElements;
+
+public class StartScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+   // Start is called before the first frame update
+   // public VisualTreeAsset mainXl;
+
+  //  public StyleSheet[] seasonal;
+    void Start()
+    {
+      //  mainXl = Resources.Load<VisualTreeAsset>("UI/mainXl");
+       // seasonal = Resources.LoadAll<StyleSheet>("UI/Seasonal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
